94d0fb1615d292648f957573ce9ba55776c3f050
Luke Arno <luke.arno@gmail.com>
2024-08-05 21:50:46 -0700

    Training

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ class Claude(LM):
         self.model = model
         self.api_key = api_key
         self.provider = "default"
-        self.kwargs = dict(temperature="0.1")
+        self.kwargs = dict(temperature=0.1)
         self.base_url = "https://api.anthropic.com/v1/messages"
 
     def basic_request(self, prompt: str, **kwargs):
